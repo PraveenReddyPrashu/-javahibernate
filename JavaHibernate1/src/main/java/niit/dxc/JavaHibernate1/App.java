868b5b17d623e9bd77f6f75d614f2669d5e3a635
Praveen Reddy <praveenreddy291@gmail.com>
2021-04-30 15:16:00 +0530

    Hibernate @annotations

---
+++ JavaHibernate1/src/main/java/niit/dxc/JavaHibernate1/App.java
@@ -0,0 +1,30 @@
+package niit.dxc.JavaHibernate1;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.cfg.Configuration;
+import org.hibernate.service.ServiceRegistry;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+        Student student = new Student(13, "Pavan", 3,33);
+        
+        Configuration configuration = new Configuration().configure().addAnnotatedClass(Student.class);
+        
+        SessionFactory sessionFactory = configuration.buildSessionFactory();
+        Session session = sessionFactory.openSession();
+        Transaction transaction = session.beginTransaction();
+        
+       session.save(student);
+       transaction.commit();
+      
+    }
+}
---
+++ JavaHibernate1/src/main/java/niit/dxc/JavaHibernate1/Student.java
@@ -0,0 +1,54 @@
+package niit.dxc.JavaHibernate1;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Transient;
+
+@Entity(name = "student_table")
+public class Student {
+	@Id
+	int _id;
+	@Transient
+	String name;
+	@Column(name = "trimester")
+	int semester;
+	int average;
+	
+	public  Student() {}
+
+	public Student(int id, String name, int semester, int average) {
+		super();
+		this._id = id;
+		this.name = name;
+		this.semester = semester;
+		this.average = average;
+	}
+	public int getId() {
+		return _id;
+	}
+	public void setId(int id) {
+		this._id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public int getSemester() {
+		return semester;
+	}
+	public void setSemester(int semester) {
+		this.semester = semester;
+	}
+	public int getAverage() {
+		return average;
+	}
+	public void setAverage(int average) {
+		this.average = average;
+	}
+	
+	
+
+}
